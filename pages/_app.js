1803c3557a3f9b9565e0e9a82cf865f10b072299
kangmoonseok87 <acuamoon1@gmail.com>
2023-11-07 14:00:19 +0900

    Next.JS반영

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Layout from '@/components/Layout';
 export default function App({ Component, pageProps }) {
   /* SunLayout이 없는 경우를 생각해서 코딩 */
   const Emptylayout = ({children}) => <>{children}</>
-  const SubLayout = Component.Layout || Emptylayout
+    const SubLayout = Component.Layout || Emptylayout
 
   return (
     <Layout>
